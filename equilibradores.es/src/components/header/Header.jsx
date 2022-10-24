fa76512f53c147932aee0a8e093d2e5d9804535b
Doplax <doplax@gmail.com>
2022-10-24 17:30:18 +0200

    Añadiendo el header

---
+++ equilibradores.es/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css"
+
+
+export default function Header(prop){
+    return(
+        <div className="header">
+            <figure>
+                <img className="logo_image" src="https://equilibradores.es/assets/images/tecsolda-logo-300x62.png" alt="logo de la empresa"></img>
+            </figure>
+            <ul className="menu">
+                <li><a href="www">Inicio</a></li>
+                <li><a href="www">Equilibradores</a></li>
+                <li><a href="www">Equilibradores Industriales</a></li>
+                <li><a href="www">Noticias</a></li>
+            </ul>
+        </div>
+    )
+
+}
