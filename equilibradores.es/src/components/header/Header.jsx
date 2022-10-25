140ae44882f5a35afad16d7c62aa26d261ee2aab
Doplax <doplax@gmail.com>
2022-10-25 09:34:15 +0200

    He creado un nuevo componente header para probar a cambiarlo despues, ahora tengo escrito el HTML

--- equilibradores.es/src/components/header/Header.jsx
+++ equilibradores.es/src/components/header/Header.jsx
@@ -3,17 +3,35 @@ import "./Header.css"
 
 export default function Header(prop){
     return(
-        <div className="header">
-            <figure>
-                <img className="logo_image" src="https://equilibradores.es/assets/images/tecsolda-logo-300x62.png" alt="logo de la empresa"></img>
-            </figure>
+        <nav>
             <ul className="menu">
-                <li><a href="www">Inicio</a></li>
-                <li><a href="www">Equilibradores</a></li>
-                <li><a href="www">Equilibradores Industriales</a></li>
-                <li><a href="www">Noticias</a></li>
+                <li className="logo"><a href="w">Creative Mind </a> </li>
+                <li className="item"><a href="w">Home </a> </li>
+                <li className="item"><a href="w">About</a> </li>
+                <li className="item has-sumbenu">
+                    <a tabIndex={0}>Services</a>
+                    <ul className="sub-menu">
+                        <li className="sub-item"> <a href="w">Design</a></li>
+                        <li className="sub-item"> <a href="w">Development</a></li>
+                        <li className="sub-item"> <a href="w">SEO</a></li>
+                        <li className="sub-item"> <a href="w">Copywriting</a></li>
+                    </ul>
+                </li>
+                <li className="item has-submenu">
+                    <a tabIndex={0}>Plans</a>
+                    <ul className="sub-menu">
+                        <li className="sub-item"><a href="w"> Freelancer</a></li>
+                        <li className="sub-item"><a href="w"> Startup</a></li>
+                        <li className="sub-item"><a href="w"> Enterprise</a></li>
+                    </ul>
+                </li>
+                <li className="item"> <a href="w"> Blog </a></li>
+                <li className="item"> <a href="w"> Contact </a></li>
+                <li className="item button"> <a href="w"> Log In </a></li>
+                <li className="item button secondary"> <a href="w"> Sing Up </a></li>
+                <li className="toggle"> <a href="w"> <i className="fas fa-bars"></i> </a></li>
             </ul>
-        </div>
+        </nav>
     )
 
 }
