2515ebad62433523ba7ecbc46cf7d2e3dd9fccdc
Doplax <doplax@gmail.com>
2023-05-15 13:23:55 +0200

    creando footer

--- equilibradores.es/src/app/page.tsx
+++ equilibradores.es/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
+
 import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,6 +12,7 @@ export default function Home() {
     <main>
         <Header></Header>
         <p>Hola mundo</p>
+        <Footer></Footer>
     </main>
   )
 }
--- equilibradores.es/src/components/Header.tsx
+++ equilibradores.es/src/components/Header.tsx
@@ -8,11 +8,16 @@ function Header() {
     return (
         <header className="bg-white shadow-md">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-            <h1 className="text-3xl font-bold text-gray-900">Mi Aplicación</h1>
+            <h1 className="text-3xl font-bold text-gray-900">Equilibradores.es</h1>
 
-            <div className="sm:hidden">
-                <button type="button" className="p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false" onClick={() => setIsOpen(!isOpen)}>
-                    {isOpen ? <AiOutlineClose className="h-6 w-6"/> : <AiOutlineMenu className="h-6 w-6"/>}
+            {/*<div className="sm:hidden">*/}
+            <div>
+                <button 
+                    type="button" 
+                    className="p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" 
+                    aria-expanded="false" 
+                    onClick={() => setIsOpen(!isOpen)}>
+                        {isOpen ? <AiOutlineClose className="h-6 w-6"/> : <AiOutlineMenu className="h-6 w-6"/>}
                 </button>
             </div>
 
