b0207d0ab8ecd03c81584cc800679bba7062b728
Doplax <doplax@gmail.com>
2023-05-17 18:52:28 +0200

    puebas de enrutado

--- equilibradores.es/src/components/Footer.tsx
+++ equilibradores.es/src/components/Footer.tsx
@@ -50,7 +50,7 @@ function Footer() {
                                         <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
                                     </svg>
                                     Filter
-                                    <svg className="-mr-1 ml-1.5 w-5 h-5" fill="currentColor" viewBox={"0 0 20 20"} xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                    <svg className="-mr-1 ml-1.5 w-5 h-5" fill="cu1rrentColor" viewBox={"0 0 20 20"} xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path clip-rule="evenodd" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
                                     </svg>
                                 </button>
